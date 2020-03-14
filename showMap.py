5617adaf374c40ef01057e6bbce39b76bf65ef79
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-14 07:59:39 +0530

    v1.0

--- showMap.py
+++ showMap.py
@@ -1,4 +1,16 @@
-import webbrowser, sys, pyperclip
+import sys
+import webbrowser
+
+import pyperclip
 
 sys.argv  # ['showMap.py, '870', 'Valencia', 'St.]
 
+# Check if command line arguments were passed
+if len(sys.argv) > 1:
+    # ['showMap.py, '870', 'Valencia', 'St.] -> '850 Valencia St'
+    address = " ".join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+# https://www.google.com/maps/place/<ADDRESS>
+webbrowser.open("https://www.google.com/maps/place/" + address)
---
+++ showmap.bat
@@ -0,0 +1,3 @@
+#place this file in your python script folder that is in path
+
+@py.exe C:\Users\crisp\Bin\Python\webScraping\showMap.py %*
